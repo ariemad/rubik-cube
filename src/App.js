78b71cdbda87ee066461ef97b7fdc67aac715f31
Daniel Bray <daniel.bray.dev@hotmail.com>
2022-11-29 18:25:51 +0000

    Add slider for rotation X

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import Cube from "./components/Cube";
 import "./css/App.scss";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { useState } from "react";
+import Cube from "./components/Cube";
+import { Button, Slider } from "@mui/material";
 
 function App() {
+  let [cubeOptions, setCubeOptions] = useState({
+    rotateX: 30,
+    rotateY: 30,
+    rotateZ: 30,
+  });
+
+  function updateCubeOptions(e) {
+    let copy = cubeOptions;
+    copy[e.target.name] = e.target.value;
+    setCubeOptions(copy);
+  }
+
   return (
     <div className="App">
-      <Cube />
+      <Cube key={cubeOptions} cubeOptions={cubeOptions} />
+      <div className="slider-container">
+        <p>Rotation on X axis {Object.values(cubeOptions)}</p>
+        <Slider
+          className="rotate-x-slider"
+          size="small"
+          defaultValue={0}
+          min={-180}
+          max={180}
+          step={1}
+          valueLabelDisplay="auto"
+          marks={[
+            { label: "0°", value: 0 },
+            { label: "90°", value: 90 },
+            { label: "-90°", value: -90 },
+            { label: "-180°", value: -180 },
+            { label: "180°", value: 180 },
+          ]}
+          onChange={updateCubeOptions}
+          name="rotateX"
+        />
+      </div>
     </div>
   );
 }
--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,5 +1,33 @@
-function Cube() {
-  return <div className="Cube"></div>;
+import { useEffect, useState } from "react";
+import "../css/Cube.scss";
+
+function Cube(props) {
+  let [cubeOptions, setCubeOptions] = useState(props.cubeOptions);
+
+  useEffect(() => {
+    setCubeOptions(props.cubeOptions);
+  }, [props.cubeOptions]);
+
+  return (
+    <div className="cube-container">
+      <div
+        className="cube"
+        style={{
+          transform: `translate(-75px, -75px) 
+          rotateX(${cubeOptions.rotateX}deg) 
+          rotateY(${cubeOptions.rotateY}deg) 
+          rotateZ(${cubeOptions.rotateZ}deg) `,
+        }}
+      >
+        <div className="front face">Front</div>
+        <div className="back face">Back</div>
+        <div className="left face">Left</div>
+        <div className="right face">Right</div>
+        <div className="top face">Top</div>
+        <div className="bottom face">Bottom</div>
+      </div>
+    </div>
+  );
 }
 
 export default Cube;
